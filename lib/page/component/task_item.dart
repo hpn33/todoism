7b223c33b0099148983527418a9021f26be61b0e
hamed <hamed.hp332@gmail.com>
2021-05-13 07:19:24 +0430

    task_item: component

--- lib/page/component/task_item.dart
+++ lib/page/component/task_item.dart
@@ -12,99 +12,25 @@ class TaskItem extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final task = useProvider(currentTask);
-
-    final state = useState(task.state);
-
-    final itemFocusNode = useFocusNode();
-    // listen to focus chances
-    useListenable(itemFocusNode);
-
-    final titleTextEditingController = useTextEditingController();
-    final titleTextFieldFocusNode = useFocusNode();
-
     return Card(
-      child: Focus(
-        focusNode: itemFocusNode,
-        onFocusChange: (focused) {
-          // print(focused);
-          if (focused) {
-            titleTextEditingController.text = task.title;
-          } else {
-            if (task.title != titleTextEditingController.text) {
-              task.title = titleTextEditingController.text;
-              task.save();
-            }
-          }
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      task.delete();
-                    },
-                  ),
-                  Checkbox(
-                    // tristate: task.state == null ? true : false,
-                    value: task.state == null ? false : task.state,
-                    onChanged: (v) {
-                      task.state = v;
-                      task.save();
-
-                      state.value = task.state;
-                    },
-                  ),
-                  Expanded(
-                    child: titleBuilder(
-                      context,
-                      task,
-                      itemFocusNode,
-                      titleTextFieldFocusNode,
-                      titleTextEditingController,
-                    ),
-                  ),
-                ],
-              ),
-              if (task.description.length > 1)
-                Column(
-                  children: [
-                    Divider(),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('description'),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Text(task.description),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              // if (tags.isNotEmpty)
-              tagsBuilder(task),
-            ],
-          ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TitleComp(),
+            DescriptionComp(),
+            tagsBuilder(),
+          ],
         ),
       ),
     );
   }
 
-  Widget tagsBuilder(Task task) {
+  Widget tagsBuilder() {
     return HookBuilder(
       builder: (BuildContext context) {
+        final task = useProvider(currentTask);
+
         final tagText = useTextEditingController();
 
         useListenable(hiveW.tags.box.listenable());
@@ -115,8 +41,10 @@ class TaskItem extends HookWidget {
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('tags'),
-                SizedBox(width: 10),
+                Container(
+                  width: 80,
+                  child: Text('tags'),
+                ),
                 Wrap(
                   children: [
                     ...task.tags
@@ -149,41 +77,179 @@ class TaskItem extends HookWidget {
       },
     );
   }
+}
 
-  Widget titleBuilder(
-    BuildContext context,
-    Task task,
-    FocusNode itemFocusNode,
-    FocusNode titleTextFieldFocusNode,
-    TextEditingController titleTextEditingController,
-  ) {
-    return GestureDetector(
-      onTap: () {
-        itemFocusNode.requestFocus();
-        titleTextFieldFocusNode.requestFocus();
+class TitleComp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final task = useProvider(currentTask);
+
+    final state = useState(task.state!);
+
+    final itemFocus = useFocusNode();
+    useListenable(itemFocus);
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Focus(
+      focusNode: itemFocus,
+      onFocusChange: (focused) {
+        if (focused) {
+          textEditingController.text = task.title;
+        } else {
+          if (task.title != textEditingController.text) {
+            task.title = textEditingController.text;
+            task.save();
+          }
+        }
       },
-      child: itemFocusNode.hasFocus
-          ? Row(
+      child: Row(
+        children: [
+          SizedBox(
+            width: 80,
+            child: Row(
               children: [
-                Expanded(
-                  child: TextField(
-                    autofocus: true,
-                    focusNode: titleTextFieldFocusNode,
-                    controller: titleTextEditingController,
-                  ),
-                ),
                 IconButton(
-                  icon: Icon(Icons.done),
+                  icon: Icon(Icons.delete),
                   onPressed: () {
-                    FocusScope.of(context).unfocus();
+                    task.delete();
+                  },
+                ),
+                Checkbox(
+                  // tristate: task.state == null ? true : false,
+                  value: task.state == null ? false : task.state,
+                  onChanged: (v) {
+                    task.state = v;
+                    task.save();
+
+                    state.value = task.state!;
                   },
                 ),
               ],
-            )
-          : Text(
-              task.title,
-              style: TextStyle(fontSize: 22),
             ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                itemFocus.requestFocus();
+                textFieldFocusNode.requestFocus();
+              },
+              child: itemFocus.hasFocus
+                  ? Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            autofocus: true,
+                            focusNode: textFieldFocusNode,
+                            controller: textEditingController,
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.done),
+                          onPressed: () {
+                            FocusScope.of(context).unfocus();
+                          },
+                        ),
+                      ],
+                    )
+                  : Text(
+                      task.title,
+                      style: TextStyle(fontSize: 22),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DescriptionComp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final task = useProvider(currentTask);
+
+    final itemFocus = useFocusNode();
+    useListenable(itemFocus);
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Column(
+      children: [
+        Divider(),
+        Focus(
+          focusNode: itemFocus,
+          onFocusChange: (focused) {
+            if (focused) {
+              textEditingController.text = task.description;
+            } else {
+              if (task.description != textEditingController.text) {
+                task.description = textEditingController.text;
+                task.save();
+              }
+            }
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 80,
+                child: Text('description'),
+              ),
+
+              // content
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    itemFocus.requestFocus();
+                    textFieldFocusNode.requestFocus();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: itemFocus.hasFocus
+                        ? Stack(
+                            children: [
+                              TextField(
+                                autofocus: true,
+                                focusNode: textFieldFocusNode,
+                                controller: textEditingController,
+                                minLines: 2,
+                                maxLines: 10,
+                              ),
+                              Positioned(
+                                right: 0,
+                                top: 0,
+                                child: IconButton(
+                                  icon: Icon(Icons.done),
+                                  onPressed: () {
+                                    FocusScope.of(context).unfocus();
+                                  },
+                                ),
+                              ),
+                            ],
+                          )
+                        : task.description.length > 0
+                            ? Text(task.description)
+                            : IconButton(
+                                icon: Icon(Icons.add),
+                                onPressed: () {
+                                  itemFocus.requestFocus();
+                                  textFieldFocusNode.requestFocus();
+                                },
+                              ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
