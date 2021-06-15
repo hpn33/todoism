cd11f0db2a492b22da24a4c40b233400d453b1a3
hamed <hamed.hp332@gmail.com>
2021-06-16 00:30:03 +0430

    use overlay for tag selector

--- lib/page/task/comp/tag_comp.dart
+++ lib/page/task/comp/tag_comp.dart
@@ -4,11 +4,71 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todoism/page/task/task_page.dart';
 import 'package:todoism/service/hive/hive_wrapper.dart';
 import 'package:todoism/service/hive/type/tag_type.dart';
+import 'package:todoism/service/hive/type/task_type.dart';
 import 'package:todoism/widget/autocomplete.dart';
 import 'package:todoism/widget/styled_box.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-class TagComp extends HookWidget {
+// class TagComp extends HookWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final task = useProvider(TaskPage.selectedTask).state;
+
+//     useListenable(hiveW.tags.box.listenable());
+//     useListenable(hiveW.taskTagRels.box.listenable());
+
+//     return StyledBox(
+//       title: 'tags',
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Wrap(
+//           children: [
+//             for (final tag in task.tags)
+//               Chip(
+//                 label: Text(tag.title),
+//                 deleteIcon: Icon(Icons.clear),
+//                 onDeleted: () => task.deleteTag(tag),
+//               ),
+//             CompositedTransformTarget(
+//               link: _layerLink,
+//               child: ActionChip(
+//                 label: Icon(Icons.add),
+//                 onPressed: () {
+//                   updateOverlayEntry(context, task);
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class TagComp extends StatefulHookWidget {
+  const TagComp({Key? key}) : super(key: key);
+
+  @override
+  _TagCompState createState() => _TagCompState();
+}
+
+class _TagCompState extends State<TagComp> {
+  final FocusNode _focusNode = FocusNode();
+
+  OverlayEntry? _overlayEntry;
+  final LayerLink _layerLink = LayerLink();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus) {
+        _overlayEntry!.remove();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final task = useProvider(TaskPage.selectedTask).state;
@@ -28,11 +88,14 @@ class TagComp extends HookWidget {
                 deleteIcon: Icon(Icons.clear),
                 onDeleted: () => task.deleteTag(tag),
               ),
-            ActionChip(
-              label: Icon(Icons.add),
-              onPressed: () => showDialog(
-                context: context,
-                builder: (BuildContext context) => TagSelector(),
+            CompositedTransformTarget(
+              link: _layerLink,
+              child: ActionChip(
+                label: Icon(Icons.add),
+                onPressed: () {
+                  updateOverlayEntry(task);
+                  _focusNode.requestFocus();
+                },
               ),
             ),
           ],
@@ -41,110 +104,57 @@ class TagComp extends HookWidget {
     );
   }
 
-  // void updateOverlay([String? query]) {
-  //   if (listSuggestionsEntry == null) {
-  //     final Size textFieldSize = (context.findRenderObject() as RenderBox).size;
-  //     final width = textFieldSize.width;
-  //     final height = textFieldSize.height;
-  //     listSuggestionsEntry = OverlayEntry(
-  //       builder: (context) {
-  //         return Positioned(
-  //           width: width,
-  //           child: CompositedTransformFollower(
-  //             link: _layerLink,
-  //             showWhenUnlinked: false,
-  //             offset: Offset(0.0, height),
-  //             child: SizedBox(
-  //               width: width,
-  //               child: Card(
-  //                 child: Column(
-  //                   children: filteredSuggestions.map(
-  //                     (suggestion) {
-  //                       return Row(
-  //                         children: [
-  //                           Expanded(
-  //                             child: InkWell(
-  //                               child: itemBuilder(context, suggestion),
-  //                               onTap: () {
-  //                                 setState(
-  //                                   () {
-  //                                     if (submitOnSuggestionTap) {
-  //                                       String newText = suggestion.toString();
-  //                                       if (textField.controller != null) {
-  //                                         textField.controller!.text = newText;
-  //                                       }
-  //                                       if (textField.focusNode != null) {
-  //                                         textField.focusNode!.unfocus();
-  //                                       }
-  //                                       if (itemSubmitted != null) {
-  //                                         itemSubmitted!(suggestion);
-  //                                       }
+  void updateOverlayEntry(Task task) {
+    RenderBox renderBox = context.findRenderObject() as RenderBox;
+    var size = renderBox.size;
+    var offset = renderBox.localToGlobal(Offset.zero);
 
-  //                                       if (clearOnSubmit) {
-  //                                         clear();
-  //                                       }
-  //                                     } else {
-  //                                       String newText = suggestion.toString();
-  //                                       textField.controller!.text = newText;
-  //                                       textChanged!(newText);
-  //                                     }
-  //                                   },
-  //                                 );
-  //                               },
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       );
-  //                     },
-  //                   ).toList(),
-  //                 ),
-  //               ),
-  //             ),
-  //           ),
-  //         );
-  //       },
-  //     );
-  //     Overlay.of(context)!.insert(listSuggestionsEntry!);
-  //   }
-
-  //   filteredSuggestions = getSuggestions(
-  //     suggestions,
-  //     itemSorter,
-  //     itemFilter,
-  //     suggestionsAmount,
-  //     query,
-  //   );
-
-  //   listSuggestionsEntry!.markNeedsBuild();
-  // }
-
-}
-
-class TagSelector extends HookWidget {
-  const TagSelector({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final task = useProvider(TaskPage.selectedTask).state;
-
-    return Dialog(
-      child: Container(
-        width: 100,
-        color: Colors.white,
-        child: AutoCompleteTextField<Tag>(
-          suggestions: hiveW.tags.all.toList(),
-          itemFilter: (a, b) => a.title.contains(b),
-          itemBuilder: (context, t) => Text(t.title),
-          textSubmitted: (text) {
-            task.addTag(Tag()..title = text);
-            Navigator.of(context).pop();
-          },
-          itemSubmitted: (tag) {
-            task.addTag(tag);
-            Navigator.of(context).pop();
-          },
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        width: size.width,
+        child: CompositedTransformFollower(
+          link: _layerLink,
+          showWhenUnlinked: false,
+          offset: Offset(-(offset.dx / 2), size.height - offset.dy),
+          child: SizedBox(
+            width: 100,
+            height: 50,
+            child: Material(
+              elevation: 4.0,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                    },
+                    icon: Icon(Icons.close),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: AutoCompleteTextField<Tag>(
+                      focusNode: _focusNode,
+                      suggestions: hiveW.tags.all.toList(),
+                      itemFilter: (a, b) => a.title.contains(b),
+                      itemBuilder: (context, t) => Text(t.title),
+                      textSubmitted: (text) {
+                        task.addTag(Tag()..title = text);
+                        _overlayEntry!.remove();
+                      },
+                      itemSubmitted: (tag) {
+                        task.addTag(tag);
+                        _overlayEntry!.remove();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
+
+    Overlay.of(context)!.insert(_overlayEntry!);
   }
 }
